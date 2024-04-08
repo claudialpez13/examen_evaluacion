94e84bc191fdc3dd6c545dadc0dde82f978d651a
claudialpez13 <clautqz@gmail.com>
2024-04-08 21:33:17 +0200

    terminado

---
+++ Code/main.py
@@ -0,0 +1,15 @@
+from data_reader import leer_datos , filtrar_calcular_media
+
+def main():
+
+    ruta_archivo = 'C:/Users/Usuario/Desktop/examen_evaluacion/Data/datos_examen.csv'
+    datos = leer_datos(ruta_archivo)
+
+    media_por_alumno = filtrar_calcular_media(datos)
+ 
+    print("Media de la columna Value para cada alumno:")
+    print(media_por_alumno)
+
+if __name__ == "__main__":
+    main()
+
